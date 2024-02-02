52f83ba139c5e44db46c052e6011c702e58cbf87
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-02-01 20:02:47 -0500

    A little redesign

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,6 +9,12 @@ module.exports = {
         gothic: ["Gothic A1", "sans-serif"],
         sans: ["Inter", defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "ghs-blue": "#010096",
+        "ghs-dark-blue": "#010046",
+        "bg-blue": "#000029",
+        "ghs-silver": "#8492A6",
+      },
     },
   },
   plugins: [],
